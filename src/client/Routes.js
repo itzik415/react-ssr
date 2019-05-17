547c8c3459a141001f290143453fda9e55adffe4
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-05-17 11:25:22 -0400

    getting the users from the server

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import UsersList, { loadData } from './components/usersList';
 
-export default () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home}/>
-            <Route path="/hi" component={() => 'Hi'} />
-        </div>
-    )
-}
+export default [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        loadData: loadData,
+        path: '/users',
+        component: UsersList,
+    },
+];
--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,23 @@
 // Startup point the client side application
+import "@babel/polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import { renderRoutes } from 'react-router-config';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
  
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>{renderRoutes(Routes)}</div>
+        </BrowserRouter>
+    </Provider>, 
     document.getElementById("root")
 );
